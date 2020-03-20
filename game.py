2b29f9a441939d249f6ad219090ff7a89468c219
Shirley-qiu <46514410+Shirley-qiu@users.noreply.github.com>
2020-03-20 18:05:15 -0400

    Update game.py

--- game.py
+++ game.py
@@ -1,7 +1,19 @@
 import pygame
 
+
+Class Health(object):
+        #def __init__(self):
+        
+        #def update(self):
+
+
+
 '''
 def collision(train_position, health):
   health +- 1
   return train_position, health
 '''
+
+def terminate():
+	pygame.quit()
+	sys.exit()
