ef87092a707dec89cb0a5b672774f230b256dfc5
Shirley-qiu <46514410+Shirley-qiu@users.noreply.github.com>
2020-03-20 19:07:52 -0400

    Update Train.py

--- Train.py
+++ Train.py
@@ -1,6 +1,8 @@
-class Train():
+class Train:
         #parameters: time it takes for train to pass, and the track its on
         def __init__(self, pass_time, track_num):
+                self.pass_time = pass_time
+                self.track_num = track_num
               
 
         
