4cea36e6a3e39d3adb4444fb9f8894b3f5222bd3
Shirley-qiu <46514410+Shirley-qiu@users.noreply.github.com>
2020-03-20 19:07:26 -0400

    Update Track.py

--- Track.py
+++ Track.py
@@ -1,5 +1,6 @@
-class Track():
-        def __init__(self):
+class Track:
+        def __init__(self, track_num):
+                self.track_num = track_num
                 self.has_train = False
         
         def train_pass():
