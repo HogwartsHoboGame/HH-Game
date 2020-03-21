75bad4962172726983bc1c2bbe161a7a0a9e0b1b
FilipeGorodscy <53091098+FilipeGorodscy@users.noreply.github.com>
2020-03-21 13:25:39 -0400

    Updated the printTracks method to use pygame

--- Tracks.py
+++ Tracks.py
@@ -1,9 +1,16 @@
+import pygame
+import Hobo
+
 class Tracks:
 
-    def __init__(self, numberOfTracks):
+    def __init__(self, numberOfTracks, screen):
         self.numberOfTracks = numberOfTracks
         self.tracks = [0] * numberOfTracks
+        self.screen = screen
 
+    def getNumberOfTracks(self):
+        return self.numberOfTracks
+    
     def setBusy(self, track):
         self.tracks[track] = 1
 
@@ -14,5 +21,17 @@ class Tracks:
         return (self.tracks[track] == 0)
 
     def printTracks(self):
-        for track in self.tracks:
-            print (" | " + str(track) + " | ", end =" ")
+        hobo = Hobo.Hobo(5, 0, self.screen)
+        trackHeight = 600
+        trackWidth = 40
+        initY = 30
+        initX = 60
+        for track in range(1, self.numberOfTracks+1):
+            pygame.draw.rect(self.screen, (150, 150,150), (track*initX, initY, trackWidth, trackHeight))
+            if self.isEmpty(track-1):
+                hobo.drawHobo((track*initX+20, 615)) 
+
+        for line in range(1, self.numberOfTracks+1):
+            pygame.draw.line(self.screen, (0, 0, 0), (0, line*initX), (700, line*initX), 3)
+
+                   
