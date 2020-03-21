ed8cded585876cca3d5090e6f61e86ed0c68e6b3
FilipeGorodscy <53091098+FilipeGorodscy@users.noreply.github.com>
2020-03-20 20:39:11 -0400

    Added methods to Track.py

--- Track.py
+++ Track.py
@@ -1,8 +1,18 @@
-class Track:
-        def __init__(self, track_num):
-                self.track_num = track_num
-                self.has_train = False
-        
-        def train_pass():
-                self.has_train = True
-                
+class Tracks:
+
+    def __init__(self, numberOfTracks):
+        self.numberOfTracks = numberOfTracks
+        self.tracks = [0] * numberOfTracks
+
+    def setBusy(self, track):
+        self.tracks[track] = 1
+
+    def setEmpty(self, track):
+        self.tracks[track] = 0
+
+    def isEmpty(self, track):
+        return (self.tracks[track] == 0)
+
+    def printTracks(self):
+        for track in self.tracks:
+            print (" | " + str(track) + " | ", end =" ")
