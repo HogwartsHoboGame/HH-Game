db0eb12bc09534a61552084e6108c812e273ac96
Filipe Gorodscy <filipegorodscy@gmail.com>
2020-04-09 12:42:24 -0400

    Fix relative path on sounds

--- Sounds.py
+++ Sounds.py
@@ -4,12 +4,12 @@ import pygame
 class Sounds():
 
     def __init__(self):
-        self.crash_sound = pygame.mixer.Sound("sounds/crash.wav")
+        self.crash_sound = pygame.mixer.Sound("./sounds/crash.wav")
 
     def background_music(self):
-        pygame.mixer.music.load("sounds/background.ogg")
+        pygame.mixer.music.load("./sounds/background.ogg")
         pygame.mixer.music.play(-1)
 
     def trains_music(self):
-        pygame.mixer.music.load("sounds/trains.ogg")
+        pygame.mixer.music.load("./sounds/trains.ogg")
         pygame.mixer.music.play(-1)
